705668481235910965f485a60a5de53b7aee4bfe
dugking <bingwei2642@qq.com>
2019-03-23 22:09:24 +0800

    添加上传文件

--- src/com/springdemo/controller/DemoController.java
+++ src/com/springdemo/controller/DemoController.java
@@ -1,7 +1,10 @@
 package com.springdemo.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -11,7 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.dugking.DTO.GetMethodParam;
@@ -177,5 +184,69 @@ public class DemoController {
 		json.setLabel(GlobalData.labelData);
 		return json;
 	}
+	@RequestMapping("/uploadHtml")
+	public String uploadHtml() {
+		return "uploadHtml";
+	}
+	 //上传文件会自动绑定到MultipartFile中
+    @RequestMapping(value="/upload",method=RequestMethod.POST)
+    public String upload(HttpServletRequest request, @RequestParam("file") MultipartFile file) throws Exception {
+
+       //如果文件不为空，写入上传路径
+       if(!file.isEmpty()) {
+           //上传文件路径
+           String path = request.getServletContext().getRealPath("/images/");
+           //上传文件名
+           String filename = file.getOriginalFilename();
+           File filepath = new File(path,filename);
+           //判断路径是否存在，如果不存在就创建一个
+           if (!filepath.getParentFile().exists()) { 
+               filepath.getParentFile().mkdirs();
+           }
+           //将上传文件保存到一个目标文件当中
+           file.transferTo(new File(path + File.separator + filename));
+           return "demo";
+       } else {
+           return "error";
+       }
+
+    }
+    
+//    /*
+//     *采用spring提供的上传文件的方法
+//     */
+//    @RequestMapping("springUpload")
+//    public String  springUpload(HttpServletRequest request) throws IllegalStateException, IOException
+//    {
+//         long  startTime=System.currentTimeMillis();
+//         //将当前上下文初始化给  CommonsMutipartResolver （多部分解析器）
+//        CommonsMultipartResolver multipartResolver=new CommonsMultipartResolver(
+//                request.getSession().getServletContext());
+//        //检查form中是否有enctype="multipart/form-data"
+//        if(multipartResolver.isMultipart(request))
+//        {
+//            //将request变成多部分request
+//            MultipartHttpServletRequest multiRequest=(MultipartHttpServletRequest)request;
+//           //获取multiRequest 中所有的文件名
+//            Iterator iter=multiRequest.getFileNames();
+//             
+//            while(iter.hasNext())
+//            {
+//                //一次遍历所有文件
+//                MultipartFile file=multiRequest.getFile(iter.next().toString());
+//                if(file!=null)
+//                {
+//                    String path="E:/"+file.getOriginalFilename();
+//                    //上传
+//                    file.transferTo(new File(path));
+//                }
+//                 
+//            }
+//           
+//        }
+//        long  endTime=System.currentTimeMillis();
+//        System.out.println("方法三的运行时间："+String.valueOf(endTime-startTime)+"ms");
+//    return "/success"; 
+//    }
 
 }
